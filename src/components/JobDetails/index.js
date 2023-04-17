4411561c44cf6eef7a1a406e0062fde3aaf99455
yuvakishore143 <vishwanathyuva143@gmail.com>
2023-04-17 05:15:34 +0000

    completed project

--- src/components/JobDetails/index.js
+++ src/components/JobDetails/index.js
@@ -46,7 +46,7 @@ class JobDetails extends Component {
   }
 
   jobParticle = (details, show, altText) => {
-    const data = 'eho'
+    const data = 'ehellho'
     return (
       <div className="first">
         <div className="jobDetails-first-section">
